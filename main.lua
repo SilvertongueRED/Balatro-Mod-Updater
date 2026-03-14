f8b8e26cb3ea83a940feb7e153dd23145542da97
SilvertongueRED <rolanderik@icloud.com>
2026-03-14 13:22:28 -0400

    Increase padding for page selector alignment - 0.45

--- main.lua
+++ main.lua
@@ -599,7 +599,7 @@ SMODS.current_mod.extra_tabs = function()
         { n = G.UIT.B, config = { h = 0.8, w = 0.1 } },
         -- Page selector
         (total_pages > 1) and {
-          n = G.UIT.R, config = { align = "cm", padding = 0.15 }, nodes = {
+          n = G.UIT.R, config = { align = "cm", padding = 0.45 }, nodes = {
             create_option_cycle {
               w = 4.5,
               scale = 0.7,
